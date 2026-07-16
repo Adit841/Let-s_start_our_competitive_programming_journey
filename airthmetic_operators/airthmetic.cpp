236b4fdd50f2465abf2c73ba62472b13faaf2b5d
Adit841 <adityagour0841@gmail.com>
2026-07-16 15:38:09 +0530

    aithmetic operation

---
+++ airthmetic_operators/airthmetic.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // airthemetic operators == (+, -, *, /)
+
+    int a = 10;
+    int b = 2;
+
+    int sum = a + b;
+    int sub = a - b; 
+    int multi = a * b;
+    int div = a / b;
+
+    cout << sum << '\n'<< sub  << '\n' << multi << '\n' << div;
+    return 0;
+}
---
+++ type_conversion/type_conversion.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    
+    /*
+        type conversion = conversion of a value to one data type to another
+                        Implicit = 
+    */
+
+    return 0;
+}
