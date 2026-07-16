9eab023b08db6af785805c25b04e993ea85f0d33
Adit841 <adityagour0841@gmail.com>
2026-07-16 18:11:08 +0530

    type conversion

--- type_conversion/type_conversion.cpp
+++ type_conversion/type_conversion.cpp
@@ -6,8 +6,22 @@ int main(){
     
     /*
         type conversion = conversion of a value to one data type to another
-                        Implicit = 
+                        Implicit = automatic
+                        Explicit = precede value with new data type for ex (int)
     */
 
+    double x = (int)3.14;
+
+    // cout << x;
+
+
+    char y = 100;
+
+    // cout << y;
+
+    int correct = 8;
+    int question = 10;
+    double score = correct/(double)question * 100;
+    cout << score << "%";
     return 0;
 }
