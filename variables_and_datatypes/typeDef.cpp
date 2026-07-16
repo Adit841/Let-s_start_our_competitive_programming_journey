1e4beca817ceaf7bdb3385335eac70e69af40ee4
Adit841 <adityagour0841@gmail.com>
2026-07-16 15:24:04 +0530

    typedef

--- variables_and_datatypes/typeDef.cpp
+++ variables_and_datatypes/typeDef.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
+#include <vector>
 
+// typedef std::vector<std::pair<std::string, int>> pairlist_t;
+
+typedef std::string text_t;
+typedef int number_t;
 int main(){
 
     /* 
     typedef = reserver keyword used to create an additional name (alias) for another data type.
-                New identifer for an existing type helps with readability and reduces types 
+                New identifer for an existing type helps with readability and reduces types
+                use when there is a clear benefit 
+                Replaced with 'using' (work better with tempelates) 
     */
+    text_t name = "Aditya";
+    number_t time = 12;
 
-    
+    std::cout << name <<'\n';
+    std::cout << time <<'\n';
+
     return 0;
 }
