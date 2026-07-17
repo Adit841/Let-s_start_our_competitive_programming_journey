d3d978347a691ccee5db04649e1d074e1977ec5f
Adit841 <adityagour0841@gmail.com>
2026-07-17 17:20:29 +0530

    math releated function

---
+++ math_releated_function/math.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+
+    int a = 20;
+    int b = 340;
+
+    int z = max(a,b);
+    int y = min(a,b);
+    int x = pow(2,4);
+    int w = sqrt(9);
+    int ab = abs(-3);
+    int ro = round(3.114);
+    
+
+    cout << z <<'\n' << y;
+    return 0;
+}
