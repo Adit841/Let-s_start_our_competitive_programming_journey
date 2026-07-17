8929460cd24373d07b597ac2c6ea99039cbfc85b
Adit841 <adityagour0841@gmail.com>
2026-07-17 17:25:56 +0530

    hypotenuse calculator

---
+++ Hypotenuse_to_find/hypotenuse.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+#include <math.h>
+
+int main(){
+    int perpendicular = 4;
+    int base = 3;
+    int sum_of_power = pow(3,2) + pow(4,2);
+    int hypotenuse = sqrt(sum_of_power);
+
+    cout << hypotenuse;
+
+    return 0;
+}
