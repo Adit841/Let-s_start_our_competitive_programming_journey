566461cc5f2f6aec4f7bfaa22253cc0d95d276b3
Adit841 <adityagour0841@gmail.com>
2026-07-28 19:50:53 +0530

    bubble sort

---
+++ 34_sorted_array/sorting.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void sort(int array[], int size);
+int main (){
+
+    int array[] = {10,9,1,4,5,3,6,7,8,2};
+    int size = sizeof(array)/ sizeof(array[0]);
+
+    sort(array, size);
+    for(int elements : array){
+        cout << elements << " ";
+    }
+    return 0;
+}
+
+
+void sort(int array[], int size){
+    int temp = 0;
+    for(int i = 0; i < size - 1; i++){
+        for(int j = 0; j < size -i -1; j++ ){
+            if(array[j] > array[j + 1]){
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+}
