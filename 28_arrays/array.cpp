7f480750a2bbb6439028a1f7ac59894203fdf03c
Adit841 <adityagour0841@gmail.com>
2026-07-28 19:11:02 +0530

    array

---
+++ 28_arrays/array.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+    /*
+        array is data structure that can hold multiple values .
+            values arer accessed by an index number 
+            "Kind of like a variable that holds multiple values"
+    */
+
+    double prices[] = {5.00, 6.99, 7.88};
+    cout << prices[0] << '\n';
+    cout << prices[1] << '\n';
+    cout << prices[2] << '\n';
+    return 0;
+}
