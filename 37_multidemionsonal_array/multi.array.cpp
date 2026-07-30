324a8de60d272fb5c3625a54b7f8103deba459bb
Adit841 <adityagour0841@gmail.com>
2026-07-30 17:35:47 +0530

    multidimensional array

---
+++ 37_multidemionsonal_array/multi.array.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string car[][3] = {{"Mustang", "Escape", "F-150"},
+                       {"Corvette", "Equinox", "Silverado"},
+                       {"Challenger", "Durango", "Ram 1500"}};
+
+    int rows = sizeof(car)/sizeof(car[0]);
+    int columns = sizeof(car[0])/sizeof(car[0][0]);
+
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < columns; j++){
+            cout << car[i][j] <<" ";
+        }
+        cout << '\n';
+    }
+    return 0;
+}
