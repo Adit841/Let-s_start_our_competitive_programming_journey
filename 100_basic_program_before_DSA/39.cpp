d3b2a6b4832be73712750a670fb16dfbb5423a71
Aditya Gour <adityagour0841@gmail.com>
2026-08-14 07:59:43 +0530

    40 done

--- 100_basic_program_before_DSA/39.cpp
+++ 100_basic_program_before_DSA/39.cpp
@@ -4,6 +4,16 @@
 using namespace std;
 
 int main(){
-
+    int n;
+    cin >> n;
+    int smallest;
+    while(n > 0){
+        int digit = n %10;
+        if(digit < smallest){
+            smallest = digit;
+        }
+        n = n / 10;
+    }
+    cout << smallest;
     return 0;
 }
--- 100_basic_program_before_DSA/40.cpp
+++ 100_basic_program_before_DSA/40.cpp
@@ -4,6 +4,23 @@
 using namespace std;
 
 int main(){
+    int n;
+    cin >> n;
+    int even_digits =  0;
+    int odd_digits = 0;
+
+    while(n > 0){
+        int digit  = n % 10;
+        if(digit % 2 == 0){
+            even_digits++;
+        }else if(digit % 2 != 0){
+            odd_digits++;
+        }
+        n = n /10;
+    }
+
+    cout << "The total number of even is : " << even_digits << endl;
+    cout << "The total number of odd is : " << odd_digits << endl;
     
     return 0;
 }
