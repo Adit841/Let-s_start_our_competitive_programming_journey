90bb3eb1c98b865c71fb6bd56946f60dd10e1681
Aditya Gour <adityagour0841@gmail.com>
2026-08-15 10:47:55 +0530

    correct th prime number question

--- 100_basic_program_before_DSA/44.cpp
+++ 100_basic_program_before_DSA/44.cpp
@@ -6,15 +6,17 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
-    int is_prime = 0;
-
-    for(int i = 2; i < n; i++){
+    int is_not_prime = 0;
+    if(n < 2){
+        is_not_prime = 1;
+    }
+    for(int i = 2; i*i < n; i++){
         if(n % i ==0){
-            is_prime++;
+            is_not_prime++;
             break;
         }
     }
-    if(is_prime == 1){
+    if(is_not_prime == 1){
         cout <<"The number is not a prime:";
     }else {
         cout << "The number is prime.";
