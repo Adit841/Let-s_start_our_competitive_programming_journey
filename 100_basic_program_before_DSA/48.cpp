b9cfdacd08a1455564aeed7381255fde190deccb
Aditya Gour <adityagour0841@gmail.com>
2026-08-15 18:21:10 +0530

    Write a program to display all Armstrong numbers from 1 to n.

--- 100_basic_program_before_DSA/48.cpp
+++ 100_basic_program_before_DSA/48.cpp
@@ -1,9 +1,43 @@
 // Write a program to display all Armstrong numbers from 1 to n.
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+
+        int temp = i;
+        int digits = 0;
+
+        // Count digits
+        while (temp > 0) {
+            digits++;
+            temp /= 10;
+        }
+
+        temp = i;
+        int armstrong = 0;
+
+        while (temp > 0) {
+            int digit = temp % 10;
+
+            int power = 1;
+
+            for (int j = 0; j < digits; j++) {
+                power *= digit;
+            }
+
+            armstrong += power;
+            temp /= 10;
+        }
+
+        if (armstrong == i) {
+            cout << i << " ";
+        }
+    }
+
     return 0;
 }
