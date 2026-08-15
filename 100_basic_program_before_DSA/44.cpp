757f0d86331ceea8b4cb00984e0283a2ba2b6366
Aditya Gour <adityagour0841@gmail.com>
2026-08-15 10:45:30 +0530

    the number is prime or not

--- 100_basic_program_before_DSA/44.cpp
+++ 100_basic_program_before_DSA/44.cpp
@@ -4,6 +4,20 @@
 using namespace std;
 
 int main(){
-    
+    int n;
+    cin >> n;
+    int is_prime = 0;
+
+    for(int i = 2; i < n; i++){
+        if(n % i ==0){
+            is_prime++;
+            break;
+        }
+    }
+    if(is_prime == 1){
+        cout <<"The number is not a prime:";
+    }else {
+        cout << "The number is prime.";
+    }
     return 0;
 }
