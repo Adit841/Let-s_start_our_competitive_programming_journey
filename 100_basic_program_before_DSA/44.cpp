079085d09f9249580ce2f1d0ef995d67f6402812
Aditya Gour <adityagour0841@gmail.com>
2026-08-15 15:22:41 +0530

    Write a program to display all prime numbers from 1 to n.

--- 100_basic_program_before_DSA/44.cpp
+++ 100_basic_program_before_DSA/44.cpp
@@ -10,7 +10,7 @@ int main(){
     if(n < 2){
         is_not_prime = 1;
     }
-    for(int i = 2; i*i < n; i++){
+    for(int i = 2; i*i <= n; i++){
         if(n % i ==0){
             is_not_prime++;
             break;
