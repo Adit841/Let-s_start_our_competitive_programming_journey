e2fcaca236585bccab1062aa41503b991c8c1889
Aditya Gour <adityagour0841@gmail.com>
2026-08-15 15:50:31 +0530

    .

--- 100_basic_program_before_DSA/47.cpp
+++ 100_basic_program_before_DSA/47.cpp
@@ -7,13 +7,20 @@ int main(){
     int n;
     cin >> n;
     int value = n;
+    int v = n;
     int arm = 0;
+    int numberOfDigits = 0;
     while(n > 0){
-        int digit = n % 10;
-        arm = arm + (digit * digit * digit);
+        int d = n % 10;
+        numberOfDigits++;
         n = n / 10;
     }
-    if(arm == value){
+    while(value > 0){
+        int digit = value % 10;
+        arm = arm + pow(digit, numberOfDigits);
+        value = value / 10;
+    }
+    if(arm == v){
         cout << "The number is palindrome";
     }else{
         cout << "The number is not a palindrome";
