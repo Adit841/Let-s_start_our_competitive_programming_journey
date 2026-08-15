92b23e256ba609a9e42e0cc40069eeb85eeac42a
Aditya Gour <adityagour0841@gmail.com>
2026-08-15 15:50:11 +0530

    Write a program to check whether a number is an Armstrong number.

--- 100_basic_program_before_DSA/47.cpp
+++ 100_basic_program_before_DSA/47.cpp
@@ -1,9 +1,22 @@
-// rite a program to check whether a number is an Armstrong number.
+// Write a program to check whether a number is an Armstrong number.
 
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
-    
+    int n;
+    cin >> n;
+    int value = n;
+    int arm = 0;
+    while(n > 0){
+        int digit = n % 10;
+        arm = arm + (digit * digit * digit);
+        n = n / 10;
+    }
+    if(arm == value){
+        cout << "The number is palindrome";
+    }else{
+        cout << "The number is not a palindrome";
+    }
     return 0;
 }
