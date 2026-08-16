fdc17f0f97ab8a32968a7026c0e0ff55c0930537
Aditya Gour <adityagour0841@gmail.com>
2026-08-16 19:24:06 +0530

    50th question done

--- 100_basic_program_before_DSA/50.cpp
+++ 100_basic_program_before_DSA/50.cpp
@@ -4,6 +4,29 @@
 using namespace std;
 
 int main(){
+    int n;
+    cin >> n;
+    int value = n;
+    
+    int sum = 0;
+    while(n > 0){
+        int digit = n % 10;
+
+        int fact = 1;
+        while(digit > 0){
+            fact = fact * digit;
+            digit--;
+        }
+
+            sum  = sum + fact;
+        n = n /10;
+    }
+
+    if(sum == value){
+        cout << "Number is strong number";
+    }else{
+        cout << "Number is not strong number";
+    }
     
     return 0;
 }
