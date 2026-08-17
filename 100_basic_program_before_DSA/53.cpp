7a4b0b1be9ea8f3265a7c5fb7c6bf27805b504b8
Aditya Gour <adityagour0841@gmail.com>
2026-08-17 17:26:16 +0530

    Write a program to find all factors (divisors) of a number n.

--- 100_basic_program_before_DSA/53.cpp
+++ 100_basic_program_before_DSA/53.cpp
@@ -4,5 +4,13 @@
 using namespace std;
 
 int main(){
+    int n;
+    cin >> n;
+
+    for(int i = 1; i <= n ; i++){
+        if(n % i == 0){
+            cout << i << " ";
+        }
+    }
     return 0;
 }
