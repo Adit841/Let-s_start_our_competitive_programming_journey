a3e445cf65216e1366781614910ed4b6ea4db9f0
Aditya Gour <adityagour0841@gmail.com>
2026-08-17 17:29:10 +0530

    another approach

--- 100_basic_program_before_DSA/59.cpp
+++ 100_basic_program_before_DSA/59.cpp
@@ -7,10 +7,11 @@ int main(){
     int n;
     cin >> n;
 
-    int sum = 0;
-    for(int i = 1; i <= n; i++){
-        sum = sum + i;
-    }
+    int sum = (n * (n + 1)) / 2;
+    // for(int i = 1; i <= n; i++){
+    //     sum = sum + i;
+    // }
     cout << sum << endl;
     return 0;
 }
+
