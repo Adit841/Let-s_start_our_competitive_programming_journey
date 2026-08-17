46d0688b9cc1b08ec1f07652d778df896efc1a01
Aditya Gour <adityagour0841@gmail.com>
2026-08-17 21:31:30 +0530

    Write a program to find the LCM of two numbers.

--- 100_basic_program_before_DSA/56.cpp
+++ 100_basic_program_before_DSA/56.cpp
@@ -3,6 +3,31 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int gcd(int a, int b){
+    if(a == 0){
+        return b;
+    }
+    if(b == 0){
+        return a;
+    }
+
+    while(a != b){
+        if( a > b){
+            a = a - b;
+        }else{
+            b = b - a;
+        }
+    }
+    return a;
+}
+
 int main(){
+    int a , b;
+    cin >> a >> b;
+    int multi = a * b;
+    int ans = gcd(a,b);
+    int lcm = multi / ans;
+
+    cout << "The LCM of " << a << " & " << b << " is " << lcm;
     return 0;
 }
