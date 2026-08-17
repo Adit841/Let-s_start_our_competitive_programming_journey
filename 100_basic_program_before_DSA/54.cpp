e1e520d6ead7e6e5a4d9a02fa6bb142cc94ab0d8
Aditya Gour <adityagour0841@gmail.com>
2026-08-17 17:40:30 +0530

    Write a program to count the number of factors of a number n.

--- 100_basic_program_before_DSA/54.cpp
+++ 100_basic_program_before_DSA/54.cpp
@@ -4,5 +4,16 @@
 using namespace std;
 
 int main(){
+    int n;
+    cin >> n;
+
+    int count = 0;
+
+    for(int i = 1; i <= n; i++){
+        if(n % i == 0){
+            count++;
+        }
+    }
+    cout << "The total factor of a number " << n << " is " << count;
     return 0;
 }
