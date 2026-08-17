2b284f12f28b6379671c442630c4d5d55a43ed8c
Aditya Gour <adityagour0841@gmail.com>
2026-08-17 17:34:18 +0530

    Write a program to find the sum of the first n terms of the Fibonacci series.

--- 100_basic_program_before_DSA/58.cpp
+++ 100_basic_program_before_DSA/58.cpp
@@ -4,5 +4,19 @@
 using namespace std;
 
 int main(){
+    int n;
+    cin >> n;
+    
+    int sum = 0;
+    int a = 0;
+    int b = 1;
+    for(int i = 1; i <= n; i++){
+        sum = sum + a;
+
+        int next = a+b;
+        a = b;
+        b = next;
+    }
+    cout << sum << endl;
     return 0;
 }
