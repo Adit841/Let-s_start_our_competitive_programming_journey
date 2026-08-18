b1c27f0d6118cdc4bf50b548f6ab05a22ce66efa
Aditya Gour <adityagour0841@gmail.com>
2026-08-18 17:36:27 +0530

    Write a program to count the number of vowels and consonants in a string.

--- 100_basic_program_before_DSA/69.cpp
+++ 100_basic_program_before_DSA/69.cpp
@@ -1,8 +1,25 @@
-// 69. Write a program to count the number of vowels and consonants in a string.
+// Write a program to count the number of vowels and consonants in a string.
 
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
+    string s;
+    cin >> s;
+    int vowel = 0;
+    int consonant = 0;
+    for(char c : s){
+        c = tolower(c);
+        if(c == 'a' || c == 'e' || c == 'i' || c ==  'o' || c == 'u'){
+            vowel++;
+        }else if(c >= 'a' && c <= 'z') {
+            consonant++;
+        }
+
+    }
+
+    cout << "Vowels: " << vowel << endl;
+    cout << "Consonants: " << consonant << endl;
+
     return 0;
 }
