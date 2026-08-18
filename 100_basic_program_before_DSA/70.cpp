a5095b6d1bf9953cae64e832f48d188fab0d3288
Aditya Gour <adityagour0841@gmail.com>
2026-08-18 17:47:41 +0530

    Write a program to count the number of words in a sentence.

--- 100_basic_program_before_DSA/70.cpp
+++ 100_basic_program_before_DSA/70.cpp
@@ -1,16 +1,22 @@
-// 70. Write a program to count the number of words in a sentence.
+//  Write a program to count the number of words in a sentence.
 
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
     string s;
-    cin >> s;
-    // cout << s.length()<<endl;
+    getline(cin, s);    
     int count = 0;
-    for(int i = 0; i < s.length(); i++){
-        count++;
+    int isWord = false;
+    for(char c : s){
+        if(c != ' ' && !isWord){
+            count++;
+            isWord = true;
+        }else if(c == ' '){
+            isWord= false;
+        }
     }
-    cout << count ;
+
+    cout << count;
     return 0;
 }
