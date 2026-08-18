8b7cfce68755ec11bf7e05263517f2ecbbd66b0d
Aditya Gour <adityagour0841@gmail.com>
2026-08-18 20:56:54 +0530

    Write a program to check whether a string is a palindrome.

--- 100_basic_program_before_DSA/72.cpp
+++ 100_basic_program_before_DSA/72.cpp
@@ -1,8 +1,20 @@
-// 72. Write a program to check whether a string is a palindrome.
+//  Write a program to check whether a string is a palindrome.
 
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
+    string s;
+    cin >> s;
+
+    string c;
+    for(int i = s.length() - 1; i>=0 ; i--){
+        c = c + s[i];
+    }
+    if(c == s){
+        cout << "String is palindrome.";
+    }else{
+        cout << "It is not palindrome ";
+    }
     return 0;
 }
