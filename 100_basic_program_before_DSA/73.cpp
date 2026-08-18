6c37cce79155a82c496be56126e88557a359e99d
Aditya Gour <adityagour0841@gmail.com>
2026-08-18 21:04:09 +0530

    Write a program to convert a string to uppercase and lowercase without inbuilt case functions.

--- 100_basic_program_before_DSA/73.cpp
+++ 100_basic_program_before_DSA/73.cpp
@@ -1,8 +1,24 @@
-// 73. Write a program to convert a string to uppercase and lowercase without inbuilt case functions.
+//  Write a program to convert a string to uppercase and lowercase without inbuilt case functions.
 
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
+    string s;
+    cin >> s;
+
+    string upper = s;
+    string lower = s;
+    for(int i = 0; i  < s.length(); i++){
+        if(upper[i] >= 'a' && upper[i] <= 'z'){
+            upper[i] = upper[i] - 32;
+        }
+
+        if(lower[i] >= 'A' && lower[i] <= 'Z'){
+            lower[i] = lower[i] + 32;
+        }
+    }
+    cout <<"Upper Case :" << upper << endl;
+    cout <<"Lower case :" << lower << endl;
     return 0;
 }
