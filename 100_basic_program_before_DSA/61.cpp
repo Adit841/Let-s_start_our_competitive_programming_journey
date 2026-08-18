45b044f655ef6be71b7bf54fdd6652c647a81a8c
Aditya Gour <adityagour0841@gmail.com>
2026-08-18 09:18:44 +0530

    Write a program to find the sum of the series 1 + 1/2 + 1/3 + ... + 1/n.

--- 100_basic_program_before_DSA/61.cpp
+++ 100_basic_program_before_DSA/61.cpp
@@ -1,8 +1,15 @@
-// 61. Write a program to find the sum of the series 1 + 1/2 + 1/3 + ... + 1/n.
+//  Write a program to find the sum of the series 1 + 1/2 + 1/3 + ... + 1/n.
 
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
+    int n;
+    cin >> n;
+    double sum = 0;
+    for(int i = 1; i <=n; i++){
+        sum = sum + (1.0/i);
+    }
+    cout << sum << endl;
     return 0;
 }
