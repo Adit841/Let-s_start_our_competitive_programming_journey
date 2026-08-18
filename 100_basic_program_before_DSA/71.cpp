4cb87b50f290b0c8294f8661ed62d3e2eddf8451
Aditya Gour <adityagour0841@gmail.com>
2026-08-18 20:54:15 +0530

    Write a program to reverse a string.

--- 100_basic_program_before_DSA/71.cpp
+++ 100_basic_program_before_DSA/71.cpp
@@ -1,8 +1,15 @@
-// 71. Write a program to reverse a string.
+//  Write a program to reverse a string.
 
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
+    string s;
+    cin >> s;
+    string c;
+    for(int i = s.length() - 1; i >=0 ; i--){
+        c = c + s[i];
+    }
+    cout << c;
     return 0;
 }
