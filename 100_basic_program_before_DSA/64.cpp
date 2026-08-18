086ad5dce6db2c2bf5f667aa04c8465afe53257a
Aditya Gour <adityagour0841@gmail.com>
2026-08-18 09:39:16 +0530

    Write a program to print an inverted right-angled triangle pattern of stars of height n.

--- 100_basic_program_before_DSA/64.cpp
+++ 100_basic_program_before_DSA/64.cpp
@@ -1,8 +1,16 @@
-// 64. Write a program to print an inverted right-angled triangle pattern of stars of height n.
+// Write a program to print an inverted right-angled triangle pattern of stars of height n.
 
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
+    int n;
+    cin >> n;
+    for(int i = n; i >= 1; i--){
+        for(int j = 1; j <= i; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
     return 0;
 }
