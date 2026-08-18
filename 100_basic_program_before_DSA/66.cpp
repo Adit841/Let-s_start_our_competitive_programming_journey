1e0f8694f38e781558ec9a0c82709eb1a68f9db2
Aditya Gour <adityagour0841@gmail.com>
2026-08-18 10:34:08 +0530

    Write a program to print a number triangle (row i contains numbers 1 to i).

--- 100_basic_program_before_DSA/66.cpp
+++ 100_basic_program_before_DSA/66.cpp
@@ -1,8 +1,16 @@
-// 66. Write a program to print a number triangle (row i contains numbers 1 to i).
+// Write a program to print a number triangle (row i contains numbers 1 to i).
 
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        for (int j = 1; j <=i; j++){
+            cout << j <<" ";
+        }
+        cout << endl;
+    }
     return 0;
 }
