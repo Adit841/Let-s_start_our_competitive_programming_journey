618392753780c3dddc4f0a0b1c361f618af1b4cd
Aditya Gour <adityagour0841@gmail.com>
2026-08-18 09:27:23 +0530

    Write a program to find the value of x raised to the power y without using inbuilt power.

--- 100_basic_program_before_DSA/62.cpp
+++ 100_basic_program_before_DSA/62.cpp
@@ -1,8 +1,18 @@
-// 62. Write a program to find the value of x raised to the power y without using inbuilt power.
+// Write a program to find the value of x raised to the power y without using inbuilt power.
 
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
+
+    int x,y;
+    cin >> x >> y;
+    int power = 1;
+
+    for(int i = 1; i <= y; i++){
+        power  = power * x;
+    }
+
+    cout << power ;
     return 0;
 }
