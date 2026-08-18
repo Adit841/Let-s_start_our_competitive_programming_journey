bf4797fe0a0f27d1e2c5db29a72211d2f0496d39
Aditya Gour <adityagour0841@gmail.com>
2026-08-18 09:54:00 +0530

    Write a program to print a pyramid pattern of stars of height n.

--- 100_basic_program_before_DSA/65.cpp
+++ 100_basic_program_before_DSA/65.cpp
@@ -1,8 +1,20 @@
-// 65. Write a program to print a pyramid pattern of stars of height n.
+// Write a program to print a pyramid pattern of stars of height n.
 
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
+    int n;
+    cin >> n;
+
+    for(int i = 1; i <=n ; i++){
+        for(int j = 1; j <=  n - i; j++){
+            cout << " ";
+        }
+        for(int j = 1; j <= 2 * i - 1; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
     return 0;
 }
