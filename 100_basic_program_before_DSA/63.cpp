27febc20675ae1fb68c875c11223d181b80cbb76
Aditya Gour <adityagour0841@gmail.com>
2026-08-18 09:30:06 +0530

    Write a program to print a right-angled triangle pattern of stars of height n.

--- 100_basic_program_before_DSA/63.cpp
+++ 100_basic_program_before_DSA/63.cpp
@@ -1,8 +1,16 @@
-// 63. Write a program to print a right-angled triangle pattern of stars of height n.
+// Write a program to print a right-angled triangle pattern of stars of height n.
 
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
     return 0;
 }
