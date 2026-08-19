70b3cc3a0c57597a328d89b0684ebc77975155e9
Aditya Gour <adityagour0841@gmail.com>
2026-08-19 20:59:17 +0530

    Write a program to sort an array in ascending order (bubble sort).

--- 100_basic_program_before_DSA/89.cpp
+++ 100_basic_program_before_DSA/89.cpp
@@ -1,8 +1,25 @@
-// 89. Write a program to sort an array in ascending order (bubble sort).
+//  Write a program to sort an array in ascending order (bubble sort).
 
 #include<bits/stdc++.h>
 using namespace std;
 
+
 int main(){
+    int arr[ ] = {23,21,43,22,34,3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    for(int i = 0; i < n - 1; i++){
+        for(int j =0; j < n - i - 1; j++){
+            if(arr[j] > arr[j + 1]){
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
     return 0;
 }
