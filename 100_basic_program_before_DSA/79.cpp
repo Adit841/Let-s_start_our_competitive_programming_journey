e8ab240ea16f186eaf2fce22c428fa5cfbbf4ee9
Aditya Gour <adityagour0841@gmail.com>
2026-08-19 10:27:41 +0530

    Write a program to toggle the case of each character in a string.

--- 100_basic_program_before_DSA/79.cpp
+++ 100_basic_program_before_DSA/79.cpp
@@ -1,8 +1,19 @@
-// 79. Write a program to toggle the case of each character in a string.
+//  Write a program to toggle the case of each character in a string.
 
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
+    string s;
+    cin >> s;
+
+    for(char &ch : s){
+        if(ch >= 'A' && ch <= 'Z'){
+            ch = ch + 32;
+        }else if(ch >= 'a' && ch <= 'z'){
+            ch = ch - 32;
+        }
+    }
+    cout << s;
     return 0;
 }
