eff52b4fa58245b2c2884063a5fc15c9a43d2e50
Aditya Gour <adityagour0841@gmail.com>
2026-08-19 23:18:45 +0530

    Write a program to check whether a number is prime, using a function/method.

--- 100_basic_program_before_DSA/92.cpp
+++ 100_basic_program_before_DSA/92.cpp
@@ -1,8 +1,25 @@
-// 92. Write a program to check whether a number is prime, using a function/method.
+//  Write a program to check whether a number is prime, using a function/method.
 
 #include<bits/stdc++.h>
 using namespace std;
 
+bool isPrime(int n){
+    
+    if(n < 2){
+        return false;
+    }
+
+    for(int i = 2; i * i < n; i++){
+        if(n % i == 0){
+            return false;
+
+        }
+    }
+    return true;
+}
 int main(){
+    int n;
+    cin >> n;
+    cout << isPrime(n);
     return 0;
 }
