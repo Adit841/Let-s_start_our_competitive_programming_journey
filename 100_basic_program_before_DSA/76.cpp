fe8cb41df767b3813b5e57d99e3cf071a18bde17
Aditya Gour <adityagour0841@gmail.com>
2026-08-19 09:33:55 +0530

    Write a program to check whether two strings are anagrams of each other.

--- 100_basic_program_before_DSA/76.cpp
+++ 100_basic_program_before_DSA/76.cpp
@@ -1,8 +1,34 @@
-// 76. Write a program to check whether two strings are anagrams of each other.
+//  Write a program to check whether two strings are anagrams of each other.
 
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
+   string s1,s2;
+   cin >> s1 >> s2;
+
+   int a[256] = {0};
+   int b[256] = {0};
+
+   for(char ch : s1){
+    a[ch]++;
+   }
+   for(char ch : s2){
+    b[ch]++;
+   }
+   bool anagram = true;
+
+   for(int i = 0; i < 256; i++){
+    if(a[i] != b[i]){
+        anagram = false;
+        break;
+    }
+   }
+   
+   if(anagram){
+    cout << "It is a anagram string ";
+   }else{
+        cout << "It is not a anagram strin ";
+   }
     return 0;
 }
