d9fe4db95cd28a79d6816fea0058ab276e5c7219
Aditya Gour <adityagour0841@gmail.com>
2026-08-19 23:36:49 +0530

    Write a program to count the number of prime digits present in a number n.

--- 100_basic_program_before_DSA/95.cpp
+++ 100_basic_program_before_DSA/95.cpp
@@ -1,8 +1,19 @@
-// 95. Write a program to count the number of prime digits present in a number n.
+//  Write a program to count the number of prime digits present in a number n.
 
 #include<bits/stdc++.h>
 using namespace std;
+int  main(){
+    int n;
+    cin >> n;
 
-int main(){
+    int count = 0;
+    while(n > 0){
+        int digit = n % 10;
+        if(digit == 2 || digit == 3 || digit == 5 || digit == 7){
+            count++;
+        }
+        n = n / 10;
+    }
+    cout << count;
     return 0;
 }
