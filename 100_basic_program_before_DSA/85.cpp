b146b5002ad50104eb50bfae4dea4087a6513888
Aditya Gour <adityagour0841@gmail.com>
2026-08-19 18:21:27 +0530

    Write a program to reverse the elements of an array.

--- 100_basic_program_before_DSA/85.cpp
+++ 100_basic_program_before_DSA/85.cpp
@@ -1,8 +1,21 @@
-// 85. Write a program to reverse the elements of an array.
+//  Write a program to reverse the elements of an array.
 
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
+    int arr[] = {1,2,3,4,5,6};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    for(int i =0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    int rev[n];
+    for(int i = 0; i < n; i++){
+        rev[i] = arr[n-i-1];
+    }
+    for(int i =0; i < n; i++){
+        cout << rev[i] << " ";
+    }
     return 0;
 }
