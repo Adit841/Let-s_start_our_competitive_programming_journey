558255af17f25056c68dd892afb7ab7d79c4f24b
Aditya Gour <adityagour0841@gmail.com>
2026-08-19 18:36:20 +0530

    Write a program to find the second largest element in an array.

--- 100_basic_program_before_DSA/86.cpp
+++ 100_basic_program_before_DSA/86.cpp
@@ -1,8 +1,25 @@
-// 86. Write a program to find the second largest element in an array.
+//  Write a program to find the second largest element in an array.
 
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
+    int arr[] = {1,2,3,4,5,6,7,8,9};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    int largest = arr[0];
+    int secondLargest = arr[0];
+
+    for(int i =1; i < n; i++){
+        if(arr[i] > largest){
+            secondLargest = largest;
+            largest = arr[i];
+        }else if(arr[i] > secondLargest && arr[i] != largest){
+            secondLargest = arr[i];
+        }
+    }
+
+    cout << "Second largest: " << secondLargest << endl;
+    cout << "Largest: " << largest << endl;
     return 0;
 }
