aa5150e9ce034023ad5ef6b9af69c8b6a31f1f81
Aditya Gour <adityagour0841@gmail.com>
2026-08-19 18:41:15 +0530

    Write a program to count the frequency of each element in an array.

--- 100_basic_program_before_DSA/87.cpp
+++ 100_basic_program_before_DSA/87.cpp
@@ -1,8 +1,22 @@
-// 87. Write a program to count the frequency of each element in an array.
+//  Write a program to count the frequency of each element in an array.
 
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
+    int arr[] = {1,2,2,4,3,3,3};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    int freq[100] = {0};
+
+    for(int c : arr){
+        freq[c]++;
+    }
+
+    for(int i = 0; i < 100; i++){
+        if(freq[i] > 0){
+            cout << i << " = " << freq[i] << endl;
+        }
+    }
     return 0;
 }
