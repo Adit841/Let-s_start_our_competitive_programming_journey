c4008901e0c2448f0b5cf388c058966327610eb2
Aditya Gour <adityagour0841@gmail.com>
2026-08-19 23:26:31 +0530

    Write a program to print all prime numbers between two given numbers a and b.

--- 100_basic_program_before_DSA/93.cpp
+++ 100_basic_program_before_DSA/93.cpp
@@ -1,8 +1,29 @@
-// 93. Write a program to print all prime numbers between two given numbers a and b.
+//  Write a program to print all prime numbers between two given numbers a and b.
 
 #include<bits/stdc++.h>
 using namespace std;
 
+bool isPrime(int n){
+    
+    if(n < 2){
+        return false;
+    }
+
+    for(int i = 2; i * i < n; i++){
+        if(n % i == 0){
+            return false;
+
+        }
+    }
+    return true;
+}
 int main(){
+    int a,b;
+    cin >> a >> b;
+    for(int i = a; i <=b; i++){
+        if(isPrime(i)){
+            cout << i <<  " ";
+        }
+    }
     return 0;
 }
