637bdd55aa182462c4f7c0845c78cffae7df70de
Aditya Gour <adityagour0841@gmail.com>
2026-08-19 18:16:27 +0530

    Write a program to search for an element in an array (linear search).

--- 100_basic_program_before_DSA/84.cpp
+++ 100_basic_program_before_DSA/84.cpp
@@ -1,8 +1,24 @@
-// 84. Write a program to search for an element in an array (linear search).
+//  Write a program to search for an element in an array (linear search).
 
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
+    int arr[] = {1,2,3,4,5,6,7,8,9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int number;
+    bool is_found = false;
+    cin >> number;
+    for(int i= 0; i < n; i++){
+        if(number == arr[i]){
+            is_found = true;
+            break;
+        }
+    }
+    if(is_found){
+        cout << "Number found";
+    }else{
+        cout << "Number not found";
+    }
     return 0;
 }
