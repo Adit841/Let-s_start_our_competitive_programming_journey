8c3272209b8cf69ffc0506a7711c0870ef32f843
Aditya Gour <adityagour0841@gmail.com>
2026-08-19 09:19:46 +0530

    Write a program to remove all spaces from a string.

--- 100_basic_program_before_DSA/75.cpp
+++ 100_basic_program_before_DSA/75.cpp
@@ -1,8 +1,17 @@
-// 75. Write a program to remove all spaces from a string.
+//  Write a program to remove all spaces from a string.
 
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
+    string s;
+    getline(cin, s);
+    for(char ch : s){
+        if(ch != ' '){
+            cout << ch;
+        }
+        
+    }
+    
     return 0;
 }
