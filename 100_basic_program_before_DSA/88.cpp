0a29b396a2edf6b8b07a14c403b23f65c14f0294
Aditya Gour <adityagour0841@gmail.com>
2026-08-19 20:50:44 +0530

    Write a program to remove duplicate elements from an array.

--- 100_basic_program_before_DSA/88.cpp
+++ 100_basic_program_before_DSA/88.cpp
@@ -1,8 +1,23 @@
-// 88. Write a program to remove duplicate elements from an array.
+// Write a program to remove duplicate elements from an array.
 
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
+    int arr[] = {1,2,2,3,4,3,3,3};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    int freq[100] = {0};
+
+    for(int c : arr){
+
+        if(freq[c] == 0){
+            cout <<c << " ";
+        }
+        freq[c]++;
+
+        
+    }
+    
     return 0;
 }
