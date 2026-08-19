fb6c49ee1224d059dee62d032c3ff987b6695b37
Aditya Gour <adityagour0841@gmail.com>
2026-08-19 09:54:06 +0530

    Write a program to replace all occurrences of a character with another character in a string.

--- 100_basic_program_before_DSA/78.cpp
+++ 100_basic_program_before_DSA/78.cpp
@@ -1,8 +1,22 @@
-// 78. Write a program to replace all occurrences of a character with another character in a string.
+//  Write a program to replace all occurrences of a character with another character in a string.
 
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
+    string s;
+    cin >> s;
+
+    int freq[256] = {0};
+
+    for(char &ch : s){
+        freq[ch]++;
+
+         if(freq[ch] % 2 == 0){
+            ch = '@';
+        }
+    }
+
+    cout << s;
     return 0;
 }
