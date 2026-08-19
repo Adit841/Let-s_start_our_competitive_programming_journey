e591ba63245f88afaa15948884abbacfdcb5ff11
Aditya Gour <adityagour0841@gmail.com>
2026-08-19 23:31:01 +0530

    Write a program to find the sum of digits of a number repeatedly until a single digit remains.

--- 100_basic_program_before_DSA/94.cpp
+++ 100_basic_program_before_DSA/94.cpp
@@ -1,8 +1,21 @@
-// 94. Write a program to find the sum of digits of a number repeatedly until a single digit remains.
+//  Write a program to find the sum of digits of a number repeatedly until a single digit remains.
 
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
+    int n;
+    cin >> n;
+
+    while(n >= 10){
+
+    int sum = 0;
+        while(n > 0){
+            sum = sum + n % 10;
+            n = n / 10;
+        }
+        n = sum;
+    }
+    cout << n;
     return 0;
 }
