dd85e64e069a361331fcb97ee21054cb162300a7
Aditya Gour <adityagour0841@gmail.com>
2026-08-19 09:42:57 +0530

    Write a program to find the first non-repeating character in a string.

--- 100_basic_program_before_DSA/77.cpp
+++ 100_basic_program_before_DSA/77.cpp
@@ -1,8 +1,23 @@
-// 77. Write a program to find the first non-repeating character in a string.
+//  Write a program to find the first non-repeating character in a string.
 
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
+    string s;
+    cin >> s;
+
+    int freq[256] = {0};
+
+    for(char ch : s){
+        freq[ch]++;
+    }
+
+    for(char ch : s){
+        if(freq[ch] == 1){
+            cout << ch;
+            break;
+        }
+    }
     return 0;
 }
