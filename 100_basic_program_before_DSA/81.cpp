29faac4c5039704634df1783b4036203559089c6
Aditya Gour <adityagour0841@gmail.com>
2026-08-19 18:03:09 +0530

    Write a program to find the sum and average of all elements in an array.

--- 100_basic_program_before_DSA/81.cpp
+++ 100_basic_program_before_DSA/81.cpp
@@ -1,8 +1,17 @@
-// 81. Write a program to find the sum and average of all elements in an array.
+//  Write a program to find the sum and average of all elements in an array.
 
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
+    int arr[] = {234,34,234,2342,4321,43};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    double sum = 0.0;
+    double average = 0.0;
+    for(int i = 0; i < n; i++){
+        sum = sum + arr[i];
+    }
+    average = sum / n;
+    cout << "Sum of array is :" << sum << " Average of array is: " << average ;
     return 0;
 }
