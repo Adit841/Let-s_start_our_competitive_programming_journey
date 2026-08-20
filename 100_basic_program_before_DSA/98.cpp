28101ff6c85b29a8f76ff61aaf37317833c6f196
Aditya Gour <adityagour0841@gmail.com>
2026-08-20 10:56:24 +0530

    Write a program to convert a decimal number into its binary equivalent.

--- 100_basic_program_before_DSA/98.cpp
+++ 100_basic_program_before_DSA/98.cpp
@@ -1,8 +1,20 @@
-// 98. Write a program to convert a decimal number into its binary equivalent.
+//  Write a program to convert a decimal number into its binary equivalent.
 
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
+    int n;
+    cin >> n;
+    vector<int> d;
+
+    while(n > 0){
+        d.push_back(n % 2);
+        n = n / 2;
+    }
+
+    for(int i = 0; i < d.size(); i++){
+        cout << d[i] << " ";
+    }
     return 0;
 }
--- 100_basic_program_before_DSA/99.cpp
+++ 100_basic_program_before_DSA/99.cpp
@@ -1,8 +1,15 @@
-// 99. Write a program to convert a binary number into its decimal equivalent.
+//  Write a program to convert a binary number into its decimal equivalent.
 
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
+    int n;
+    cin >> n;
+
+    int count = 0;
+    for(int i = 0; i < n; i++){
+        
+    }
     return 0;
 }
