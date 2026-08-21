1624a6594667143122d0426b6901855987d0cb69
Aditya Gour <adityagour0841@gmail.com>
2026-08-21 18:28:23 +0530

    sum of n number and factorial using recursion

---
+++ STRIVER_A2Z_SHEET/Recursion/3recursion.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Parameterized way
+// void sumOfN(int i, int sum){
+//     if(i == 0){
+//         cout << sum;
+//         return;
+//     }
+//     sumOfN(i - 1, sum + i);
+// }
+
+// functional way
+// int sumOfN(int n){
+//     if(n  == 0){
+//         return 0;
+//     }
+//     return n + sumOfN(n-1);
+// }
+
+// Task in the end of the video
+
+// int factorial(int n){
+//     if(n  == 0) return 1;
+//     return n * factorial(n - 1);
+// }
+int main(){
+    int n;
+    cin >> n;
+    // sumOfN(n,0);
+    // cout << sumOfN(n);
+    // cout << factorial(n);
+}
