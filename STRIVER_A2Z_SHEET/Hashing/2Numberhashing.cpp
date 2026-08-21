703f391d69729856735e74d5d582b155c07e1cee
Aditya Gour <adityagour0841@gmail.com>
2026-08-21 23:51:00 +0530

    number hashing

---
+++ STRIVER_A2Z_SHEET/Hashing/2Numberhashing.cpp
@@ -0,0 +1,21 @@
+    #include<bits/stdc++.h>
+    using namespace std;
+
+    int main(){
+        int arr[] = {1,2,3,1,2,4};
+        int n = sizeof(arr)/sizeof(arr[0]);
+
+        int hash[13] = {0};
+
+        for(int i : arr){
+            hash[i]++;
+        }
+
+        for(int i = 0; i < n; i++){
+            if(hash[i] > 0){
+            cout << i << " = " << hash[i] << endl;
+        }
+        }
+
+        return 0;
+    }
