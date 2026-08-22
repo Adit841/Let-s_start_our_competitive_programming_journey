4e1da56d38ff56fa7c17fb21d6f970037e627e12
Aditya Gour <adityagour0841@gmail.com>
2026-08-22 16:32:45 +0530

    Bubble sort

--- STRIVER_A2Z_SHEET/Sorting/Bubblesort.cpp
+++ STRIVER_A2Z_SHEET/Sorting/Bubblesort.cpp
@@ -1,18 +1,28 @@
-//Bubble Sort = 
+//Bubble Sort = Time Complexity = O(n^2) Worst and Average  and for Best it will be O(n)
 
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int arr[] = {34,223,23,4,2,1};
-    int n = sizeof(arr)/sizeof(arr[0]);
+void bubble_sort(int arr[], int n){
     for(int i = 0; i <n; i++){
-        for(int j =0; j < n; j++){
+        int didSwap = 0;
+        for(int j =0; j < n - i - 1; j++){
             if(arr[j] > arr[j+1]){
-                swap(arr[j],arr[j+1]);
+                int temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+                didSwap++;
             }
         }
+        if(didSwap == 0){
+            return ;
+        }
     }
+}
+int main(){
+    int arr[] = {1,2,3,4,5,6};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    bubble_sort(arr,n);
 
     for(int i =0; i < n; i++){
         cout << arr[i] << " ";
